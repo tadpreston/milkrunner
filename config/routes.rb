c7aca2ced703de375eb856df18bd7b01930a7860
Tad Preston <tpreston@udsamerica.com>
2014-02-10 16:13:17 -0600

    Generated the home and help controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Milkrunner::Application.routes.draw do
+  get "pages/home"
+  get "pages/help"
+
+  root 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
