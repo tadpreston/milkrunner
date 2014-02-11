74696f23695054c2ea08222b0971c6fb5123291e
Tad Preston <tpreston@udsamerica.com>
2014-02-10 21:55:58 -0600

    Refactor of tests for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Milkrunner::Application.routes.draw do
-  get "pages/home"
-  get "pages/help"
 
   root 'pages#home'
+
+  match '/help',  to: 'pages#help',   via: 'get'
+  match '/about', to: 'pages#about',  via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,10 +1,45 @@
 require 'spec_helper'
 
 describe 'Site pages' do
+
+  subject { page }
+
+  shared_examples_for "all pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe 'Home page' do
-    it "should have the content 'Milkrunner'" do
-      visit '/pages/home'
-      expect(page).to have_content('Milkrunner')
-    end
+
+    before { visit root_path }
+
+    let(:heading) { 'MilkRunner' }
+    let(:page_title) { '' }
+
+    it_should_behave_like 'all pages'
+    it { should_not have_title('| Home') }
+
+  end
+
+  describe 'Help page' do
+
+    before { visit help_path }
+
+    let(:heading) { 'Help' }
+    let(:page_title) { heading }
+
+    it_should_behave_like 'all pages'
+
+  end
+
+  describe 'About page' do
+
+    before { visit about_path }
+
+    let(:heading) { 'About' }
+    let(:page_title) { heading }
+
+    it_should_behave_like 'all pages'
+
   end
 end
